397b17b1830177748fa1e0dd3467ffc79c92c340
Henning A. L <31424568+letsCodeMyLife@users.noreply.github.com>
2017-09-30 11:18:39 +0200

    Update mapsOpener.py

--- mapsOpener.py
+++ mapsOpener.py
@@ -3,8 +3,7 @@ from tkinter import filedialog
 
 
 def openMap():
-    file = filedialog.askopenfilename(initialdir="./maps",
-                                      filetypes=(("Map file", "*.map"),
+    file = filedialog.askopenfilename(filetypes=(("Map file", "*.map"),
                                                   ("All files", "*.*")),
                                       title="Choose a map")
 
